53a6db95cefb91e89b7cb545f2f08f6f19c6db45
leonardoramosc <leorrc96@gmail.com>
2021-01-14 17:27:56 -0400

    Initial commit

---
+++ calculator.go
@@ -0,0 +1,76 @@
+package mycalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func (calc) operate(input string, op string) float64 {
+
+	values := strings.Split(input, op)
+
+	op1 := parse(values[0])
+	op2 := parse(values[1])
+
+	switch op {
+	case "+":
+		fmt.Println(op1 + op2)
+		return op1 + op2
+	case "-":
+		fmt.Println(op1 - op2)
+		return op1 - op2
+	case "*":
+		fmt.Println(op1 * op2)
+		return op1 * op2
+	case "/":
+		fmt.Println(op1 / op2)
+		return op1 / op2
+	default:
+		fmt.Println(op, " operator is not supported")
+		return 0
+	}
+}
+
+func formatInput(str string) string {
+	splitted := strings.Split(strings.TrimSpace(str), " ")
+	formatted := strings.Join(splitted, "")
+
+	return formatted
+}
+
+func getUserInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Ingresa operacion:")
+
+	scanner.Scan()
+
+	return formatInput(scanner.Text())
+}
+
+func getOperator(operation string) string {
+	operators := []string{"+", "-", "*", "/"}
+	var op string
+
+	for i := 0; i < len(operators); i++ {
+		operator := operators[i]
+
+		if strings.Contains(operation, operator) {
+			index := strings.Index(operation, operator)
+			splitted := strings.Split(operation, "")
+			op = splitted[index]
+		}
+	}
+
+	return op
+}
+
+func parse(str string) float64 {
+	op, _ := strconv.ParseFloat(str, 64)
+
+	return op
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leonardoramosc/mycalculator
+
+go 1.15
